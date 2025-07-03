c0134392ba56ee3790fa28953fea9aae260ff413
stephuofa <yearley@ualberta.ca>
2025-07-03 12:07:57 -0600

    update unix build script

--- build.sh
+++ build.sh
@@ -1,8 +1,17 @@
 # run from top level (sprint3)
 
+
 rm -rf build
 mkdir build
 cd build
+
+for arg in "$@"; do
+    if [ "$arg" = "-min" ]; then
+        cmake -DMAKE_TESTS:BOOL=ON ..
+        break
+    fi
+done
+
 cmake ..
 make
 cd ..
