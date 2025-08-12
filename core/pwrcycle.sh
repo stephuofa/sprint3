2c2d7a931147fdceaa52c8e3c1834b5539a1dc50
Jasmine <jamah2@ualberta.ca>
2025-08-12 09:17:47 -0600

    added pwrcycle.sh

--- core/pwrcycle.sh
+++ core/pwrcycle.sh
@@ -1,1 +1,20 @@
-# TODO
+#!/bin/bash
+
+# Usage: ./pwrcycle.sh <pin_number> <duration_seconds>
+
+PIN=$1
+DURATION=$2
+
+if [ -z "$PIN" ] || [ -z "$DURATION" ]; then
+          echo "Usage: $0 <pin_number> <duration_seconds>"
+            exit 1
+fi
+
+# Turn on GPIO
+gpioset gpiochip0 $PIN=1
+
+# Sleep for the duration
+sleep $DURATION
+
+# Turn off GPIO
+gpioset gpiochip0 $PIN=0
